42097028ab9d1385a8055a1b80b9b10c0ad2c600
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-01-23 14:47:34 -0800

    navbar component layout

---
+++ src/components/CartButtons.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CartButtons = () => {
+  return <h3>cart buttons</h3>;
+};
+
+export default CartButtons;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,113 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { FaBars } from "react-icons/fa";
+
+import { CartButtons } from "../components";
+import { links } from "../utils/constants";
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <NavContainer>
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <h3 className="logo">
+              i<span>ll</span>umination
+            </h3>
+          </Link>
+          <button type="button" className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            const { id, text, url } = link;
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        <CartButtons />
+      </div>
+    </NavContainer>
+  );
 };
+
+const NavContainer = styled.nav`
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .logo {
+    color: var(--clr-black);
+    font-family: "Dancing Script", cursive;
+
+    span {
+      color: var(--clr-primary-5);
+    }
+  }
+
+  .nav-center {
+    width: 90vw;
+    margin: 0 auto;
+    max-width: var(--max-width);
+  }
+
+  .nav-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .nav-toggle {
+    border: 2px solid green;
+    background: transparent;
+    color: var(--clr-primary-5);
+    cursor: pointer;
+    svg {
+      font-size: 2rem;
+    }
+  }
+
+  .nav-links {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    .nav-toggle {
+      display: none;
+    }
+
+    .nav-center {
+      display: grid;
+      grid-template-columns: auto 1fr auto;
+    }
+
+    .nav-links {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      li {
+        margin: 0 0.5rem;
+      }
+
+      a {
+        color: var(--clr-grey-3);
+        font-size: 1rem;
+        text-transform: capitalize;
+        letter-spacing: var(--spacing);
+        padding: 0.5rem;
+        &:hover {
+          border-bottom: 2px solid var(--clr-primary-7);
+        }
+      }
+    }
+  }
+`;
 
 export default Navbar;
