0e109e95e50be770d674549e4fc119a3530ff35c
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-01-23 15:51:53 -0800

    footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <Wrapper>
+      <h5>
+        &copy; {new Date().getFullYear()} <span>illumination</span>
+      </h5>
+      <h5>all rights reserved</h5>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.footer`
+  height: 5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: var(--clr-black);
+
+  span {
+    color: var(--clr-primary-5);
+  }
+
+  h5 {
+    color: var(--clr-white);
+    margin: 0.1rem;
+    font-weight: 400;
+    line-height: 1.25;
+  }
+`;
 
 export default Footer;
